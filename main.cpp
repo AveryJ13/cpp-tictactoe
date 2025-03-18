ac0d310f251ada0d46b663a748aa1fbe92fd5290
Avery <sahrjohnson13@gmail.com>
2025-03-18 00:29:05 -0600

    basis starter framework command line(with some pointer practice

--- main.cpp
+++ main.cpp
@@ -2,8 +2,56 @@
 
 using namespace std;
 
+void drawGameBoard(char *gameArray);
+void inputMove(char *gameArray, string *findIndex, string input, char *playerType);
+void switchPlayer(char *playerType);
+
 int main()
 {
-    cout << "git initialization" << endl;
+    char gameArray[9] = {'*', '*', '*', '*', '*', '*', '*', '*', '*'};
+    string findIndex[9] = {"a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"};
+    char playerType = 'X';
+    string input;
+    bool gameLoop = true;
+
+    while (gameLoop == true)
+    {
+        drawGameBoard(gameArray);
+        cout << playerType << " to move, input cell above (ex) a1" << endl;
+        cin >> input;
+        inputMove(gameArray, findIndex, input, &playerType);
+        switchPlayer(&playerType);
+    }
     return 0;
+}
+
+void drawGameBoard(char *gameArray)
+{
+    cout << "  1 2 3" << endl;
+    cout << "a " << gameArray[0] << " " << gameArray[1] << " " << gameArray[2] << endl;
+    cout << "b " << gameArray[3] << " " << gameArray[4] << " " << gameArray[5] << endl;
+    cout << "c " << gameArray[6] << " " << gameArray[7] << " " << gameArray[8] << endl;
+}
+
+void inputMove(char *gameArray, string *findIndex, string input, char *playerType)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (findIndex[i] == input)
+        {
+            gameArray[i] = *playerType;
+            return;
+        }
+    }
+    cout << "please enter a valid input" << endl;
+}
+
+void switchPlayer(char *playerType)
+{
+    if (*playerType == 'X')
+    {
+        *playerType = 'O';
+    }
+    else
+        *playerType = 'X';
 }
